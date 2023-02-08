34dbbc5ba1c8267eea3f4566c5a55dbc15cc6171
roy2504 <122998913+roy2504@users.noreply.github.com>
2023-02-07 23:07:06 -0500

    Fixed the add() function at line 51 to include num3.

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -47,8 +47,8 @@ void add() {
 	scanf_s("%lf", &num2);
 	printf("Enter the third value:");
 	scanf_s("%lf", &num3);
-	result = num1 + num2+num3;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	result = num1 + num2 + num3;
+	printf("%lf + %lf + %lf = %lf\n", num1, num2, num3, result);
 }
 
 void subtract() {
